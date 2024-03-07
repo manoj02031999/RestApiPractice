c90ffb97493753ea44c7d251d50c5c0f5b7fb72c
manoj <manojkumarmy231@gmail.com>
2024-03-07 11:51:41 +0530

    get Api link from Json File using Json file using jacksonbinder

---
+++ src/main/java/utils/JsonUtils.java
@@ -0,0 +1,18 @@
+package utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class JsonUtils {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    public static Map<String,String> getJsonDataAsMap(String jsonFileName) throws IOException {
+        String completeJsonFilePath = System.getProperty("user.dir")+"/src/test/resources" + jsonFileName;
+        Map<String,String> data = (Map<String, String>) objectMapper.readValue(new File(completeJsonFilePath), new TypeReference<Object>() {});
+        return data;
+    }
+
+}
--- src/test/java/hotelbooking/BookingTest.java
+++ src/test/java/hotelbooking/BookingTest.java
@@ -3,15 +3,19 @@ package hotelbooking;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.RestUtils;
+import restUtils.RestUtils;
+import utils.JsonUtils;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BookingTest {
     @Test
-    public void createBooking(){
-        String endpoint = "https://restful-booker.herokuapp.com/booking";
+    public void createBooking() throws IOException {
+        String env = System.getProperty("env") == null ? "qa": System.getProperty("env");
+        Map<String,String> data = JsonUtils.getJsonDataAsMap("/"+env+"/hotelApiData.json");
+        String endpoint = data.get("createHotelBookingEndpoint");
         Map<String, Object> requestPayLoad = Payloads.getCreateHotelBookingPayloadFromMap("Manoj","Kumar","250","50","2024-03-01","2024-03-06","Dinner");
        Response response = RestUtils.performPost(endpoint, requestPayLoad, new HashMap<>());
         Assert.assertEquals(response.statusCode(),200);
