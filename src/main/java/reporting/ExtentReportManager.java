7f675ba850f4183e4a85770a4be053042a7ac290
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-03-13 17:07:55 +0530

    Create ThreadLocal ExtentTest & Custom Logging Methods

--- src/main/java/reporting/ExtentReportManager.java
+++ src/main/java/reporting/ExtentReportManager.java
@@ -1,6 +1,8 @@
 package reporting;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -27,5 +29,18 @@ public class ExtentReportManager {
         String formattedTime = dateTimeFormatter.format(localDateTime);
         String reportName = "TestReport" +formattedTime+ ".html";
         return reportName;
+    }
+
+    public  static  void logPassDetails(String log){
+        SetUp.extentTest.get().pass(MarkupHelper.createLabel(log, ExtentColor.GREEN));
+    }
+    public  static  void logFailDetails(String log){
+        SetUp.extentTest.get().fail(MarkupHelper.createLabel(log, ExtentColor.RED));
+    }
+    public  static  void logInfoDetails(String log){
+        SetUp.extentTest.get().info(MarkupHelper.createLabel(log, ExtentColor.GREY));
+    }
+    public  static  void logWarningDetails(String log){
+        SetUp.extentTest.get().warning(MarkupHelper.createLabel(log, ExtentColor.YELLOW));
     }
 }
