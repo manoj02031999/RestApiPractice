d373cbbfaddee752016f14f2a4f686357e278f16
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-03-14 06:21:32 +0530

    Added Log Request & Response Details In Extent Report

--- src/main/java/restUtils/RestUtils.java
+++ src/main/java/restUtils/RestUtils.java
@@ -3,27 +3,48 @@ package restUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.QueryableRequestSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
+import reporting.ExtentReportManager;
 
 import java.util.Map;
 
 public class RestUtils {
-    public static Response performPost(String endpoint, String requestPayLoad, Map<String,String> headers){
-        return RestAssured.given().log().all()
+    private static RequestSpecification getRequestSpecification(String endpoint, Object requestPayLoad, Map<String,String> headers){
+        return RestAssured.given()
                 .baseUri(endpoint)
                 .headers(headers)
                 .contentType(ContentType.JSON)
-                .body(requestPayLoad)
-                .post()
-                .then().log().everything().extract().response();
+                .body(requestPayLoad);
+    }
+
+    private static void printRequestLogInReport(RequestSpecification requestSpecification){
+        QueryableRequestSpecification queryableRequestSpecification = SpecificationQuerier.query(requestSpecification);
+        ExtentReportManager.logInfoDetails("End point is : "+ queryableRequestSpecification.getBaseUri());
+        ExtentReportManager.logInfoDetails("Method is : "+ queryableRequestSpecification.getMethod());
+        ExtentReportManager.logInfoDetails("End Headers are : "+ queryableRequestSpecification.getHeaders().asList().toString());
+        ExtentReportManager.logInfoDetails("Request Body is : "+ queryableRequestSpecification.getBody());
+    }
+
+    private static void printResponseLogInReport(Response response){
+        ExtentReportManager.logInfoDetails("Response Status is : "+ response.getStatusCode());
+        ExtentReportManager.logInfoDetails("Response Headers are : "+ response.getHeaders().asList().toString());
+        ExtentReportManager.logInfoDetails("Response Body is : "+ response.getBody());
+    }
+    public static Response performPost(String endpoint, String requestPayLoad, Map<String,String> headers){
+        RequestSpecification requestSpecification = getRequestSpecification(endpoint,requestPayLoad,headers);
+        Response response = requestSpecification.post();
+        printRequestLogInReport(requestSpecification);
+        printResponseLogInReport(response);
+        return response;
     }
 
     public static Response performPost(String endpoint, Map<String, Object> requestPayLoad, Map<String,String> headers){
-        return RestAssured.given().log().all()
-                .baseUri(endpoint)
-                .headers(headers)
-                .contentType(ContentType.JSON)
-                .body(requestPayLoad)
-                .post()
-                .then().log().everything().extract().response();
+        RequestSpecification requestSpecification = getRequestSpecification(endpoint,requestPayLoad,headers);
+        Response response = requestSpecification.post();
+        printRequestLogInReport(requestSpecification);
+        printResponseLogInReport(response);
+        return response;
     }
 }
