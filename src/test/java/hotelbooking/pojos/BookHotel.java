48144a8fb35311598c2f5b71a19a54366ce932a2
Manoj Kumar M Y <manojkumarmy231@gmail.com>
2024-03-19 19:57:55 +0530

    POJO Class With Default Values| @Builder| toBuilder

--- src/test/java/hotelbooking/pojos/BookHotel.java
+++ src/test/java/hotelbooking/pojos/BookHotel.java
@@ -1,17 +1,23 @@
 package hotelbooking.pojos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import utils.RandomDataGenerator;
+import utils.RandomDataTypeNames;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class BookHotel {
-    private String firstname;
-    private String lastname;
-    private String totalprice;
-    private Boolean depositpaid;
-    private String checkin;
-    private String checkout;
-    private String additionalneeds;
+    private String firstname = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.FIRSTNAME);
+    private String lastname = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.LASTNAME);
+    private String totalprice = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.TOTALPRICE);
+    private Boolean depositpaid = RandomDataGenerator.getRandomDataForDeposit(RandomDataTypeNames.DEPOSITPAID);
+    private String checkin = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.CHECKIN);
+    private String checkout = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.CHECKOUT);
+    private String additionalneeds = RandomDataGenerator.getRandomDataFor(RandomDataTypeNames.ADDITIONALNEEDS);
 
 }
